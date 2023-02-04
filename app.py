f67b85fe22ed17a5524e34cab6d2999816fba8f0
Marvin Zhang <tikazyq@163.com>
2023-02-05 00:16:37 +0800

    ci: attempt to fix ci/cd issue

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from argparse import Namespace
 from webspot.cmd.web import cmd_web
 
 if __name__ == '__main__':
-    args = Namespace(port=8080, log_level='debug')
+    args = Namespace(port=8000, log_level='debug')
     cmd_web(args)

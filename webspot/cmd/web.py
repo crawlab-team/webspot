860d8de774574d4affce5acba09ed09bf9b3020c
Marvin Zhang <tikazyq@163.com>
2023-02-05 13:14:47 +0800

    ci: attempt to fix web failure issue

--- webspot/cmd/web.py
+++ webspot/cmd/web.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 def cmd_web(args):
-    uvicorn.run('webspot.web.app:app', reload=True, port=args.port, log_level=args.log_level)
+    uvicorn.run('webspot.web.app:app', reload=True, host=args.host, port=args.port, log_level=args.log_level)

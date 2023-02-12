167c8044c1b47221cb7580f63487b5a27e7f42cf
Joey <2504657518@qq.com>
2023-02-12 23:21:29 +0800

    feat: optimized loading status, added skeleton and fixed some bugs

--- webspot/web/static/js/app.js
+++ webspot/web/static/js/app.js
@@ -1,4 +1,5 @@
 const {ref, computed, watch, onBeforeMount} = Vue;
+const {ElMessage} = ElementPlus;
 import TopNavbar from './components/top-navbar.js';
 import NavSidebar from './components/nav-sidebar.js';
 import PreviewContainer from './components/preview-container.js';
@@ -14,6 +15,7 @@ export default {
     const results = ref('');
     const html = ref('');
     const error = ref('');
+    const isLoading = ref(false);
 
     const htmlString = computed(() => {
       return atob(props.html);
@@ -24,10 +26,18 @@ export default {
     };
 
     const onSubmit = async () => {
-      const res = await axios.post(`/detect`, {url: url.value});
-      results.value = res.data.results;
-      html.value = res.data.html;
-      error.value = res.data.error;
+      try {
+        isLoading.value = true
+        const res = await axios.post(`/detect`, {url: url.value});
+        if(res) isLoading.value = false
+        results.value = res.data.results;
+        html.value = res.data.html;
+        error.value = res.data.error;
+      } catch (error) {
+        isLoading.value = false
+        const {detail} = error?.response?.data
+        ElMessage({message: detail, type: 'warning', offset: 80})
+      }
     };
 
     onBeforeMount(async () => {
@@ -43,6 +53,16 @@ export default {
       return props.error.split('\n').join('<br>');
     });
 
+    watch(() => url.value, (val) => {
+      // if the input url changes, the page data should be cleared.
+      if(val) {
+        results.value = ''
+        html.value = ''
+        error.value = ''
+        isLoading.value = false
+      }
+    })
+
     return {
       url,
       results,
@@ -52,10 +72,11 @@ export default {
       onUrlChange,
       onSubmit,
       formattedError,
+      isLoading,
     };
   },
   template: `
-<top-navbar :url="url" @url-change="onUrlChange" @submit="onSubmit"/>
+<top-navbar :url="url" @url-change="onUrlChange" @submit="onSubmit" :is-loading="isLoading"/>
 <div class="main-container">
   <template v-if="url">
     <!--error-->
@@ -68,20 +89,44 @@ export default {
     </template>
     <!--./error-->
 
+    <!--skeleton-->
+    <template v-else-if="isLoading">
+      <el-skeleton style="padding-top: 2%;" animated :throttle="500">
+        <template #template>
+          <div style="display: flex;">
+            <el-skeleton style="width: 240px; margin-left: 5%;" :rows="30" />
+            <el-skeleton style="flex: 1; margin: 0 10%;" :rows="30" />
+          </div>
+        </template>
+      </el-skeleton>
+    </template>
+    <!--./skeleton-->
+
     <!--results-->
     <template v-else-if="results && html">
       <nav-sidebar :results="results" @submit="onSubmit" @change="onUrlChange"/>
       <preview-container :html="html"/>
     </template>
     <!--./results-->
+
+    <!--empty-->
+    <template v-else>
+      <el-empty style="width: 100%; padding-bottom: 15%;" description="Please enter the URL to start">
+        <div class="url-wrapper" style="display: flex; width: 640px;">
+          <el-input v-model="url" placeholder="Please enter the URL" @keyup.enter="onSubmit"/>
+          <el-button type="primary" @click="onSubmit" style="margin-left: 5px" :loading="isLoading">Submit</el-button>
+        </div>
+      </el-empty>
+    </template>
+    <!--./empty-->
   </template>
 
   <!--no-url-->
   <template v-else>
-    <el-empty style="width: 100%" description="Please enter the URL to start" style="padding-bottom: 15%">
+    <el-empty style="width: 100%; padding-bottom: 15%;" description="Please enter the URL to start">
       <div class="url-wrapper" style="display: flex; width: 640px;">
         <el-input v-model="url" placeholder="Please enter the URL" @keyup.enter="onSubmit"/>
-        <el-button type="primary" @click="onSubmit" style="margin-left: 5px">Submit</el-button>
+        <el-button type="primary" @click="onSubmit" style="margin-left: 5px" :loading="isLoading">Submit</el-button>
       </div>
     </el-empty>
   </template>

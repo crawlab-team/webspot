a2e968433bb8eb682b1ba1ea336ecebfcd42043e
Marvin Zhang <tikazyq@163.com>
2023-02-25 14:32:02 +0800

    refactor: updated connect.py
    AB#68

--- webspot/db/connect.py
+++ webspot/db/connect.py
@@ -1,8 +1,14 @@
-from mongoengine import connect as mongo_connect
+import os
 
-from webspot.db.engine import get_connection_str
+from mongoengine import connect as mongo_connect
 
 
 def connect():
     conn_str = get_connection_str()
     mongo_connect(host=conn_str)
+
+
+def get_connection_str():
+    if os.environ.get('WEBSPOT_DATABASE_URL'):
+        return os.environ.get('WEBSPOT_DATABASE_URL')
+    return 'mongodb://localhost:27017/webspot'

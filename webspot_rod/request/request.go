1c9860a58bffee3733a5e3213c501d9423f5723c
Marvin Zhang <tikazyq@163.com>
2023-04-01 09:19:43 +0800

    fix: browser issue

--- webspot_rod/request/request.go
+++ webspot_rod/request/request.go
@@ -1,12 +1,11 @@
 package request
 
 import (
-	"github.com/go-rod/rod"
 	"time"
 )
 
 func GetHtml(requestUrl string, duration int) string {
-	page := rod.New().MustConnect().MustPage()
+	page := GetBrowser().MustConnect().MustPage()
 	page.MustNavigate(requestUrl).MustWaitLoad()
 	if duration > 0 {
 		time.Sleep(time.Duration(duration) * time.Second)
